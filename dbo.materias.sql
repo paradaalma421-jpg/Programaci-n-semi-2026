97910eaed9c5cffd1aebbefb8c61f7e857d75fee
Alma Iveth Parada Campos <paradaalma421@gmail.com>
2026-09-19 17:31:41 -0600

    TABLAS COMPUTO 2

--- dbo.materias.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[matriculas] (
-    [idMatriula ] INT       NOT NULL,
-    [idAlumno]    INT       NOT NULL,
-    [fecha ]      DATE      NOT NULL,
-    [periodo ]    CHAR (10) NULL,
-    PRIMARY KEY CLUSTERED ([idMatriula ] ASC)
-);
-
