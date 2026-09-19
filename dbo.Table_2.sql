f3ca21b1990c2c9ee87d3ead735f4c5a9af60538
paradaalma421-jpg <paradaalma421@gmail.com>
2026-09-18 22:58:03 -0300

    Add files via upload

---
+++ dbo.Table_2.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[idMatriula ] INT NOT NULL PRIMARY KEY, 
+    [idAlumno] INT NOT NULL, 
+    [fecha ] DATE NOT NULL, 
+    [periodo ] CHAR(10) NULL
+)
---
+++ dbo.alumnos.sgl.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[matriculas] (
+    [idMatriula ] INT       NOT NULL,
+    [idAlumno]    INT       NOT NULL,
+    [fecha ]      DATE      NOT NULL,
+    [periodo ]    CHAR (10) NULL,
+    PRIMARY KEY CLUSTERED ([idMatriula ] ASC)
+);
+
