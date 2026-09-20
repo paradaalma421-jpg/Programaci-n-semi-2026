31ad85adc20b6cc7d95ce08a8c91fbb66abb3123
Alma Iveth Parada Campos <paradaalma421@gmail.com>
2026-09-19 20:45:04 -0600

    tablas computo2

--- dbo.alumnos.sgl.sql
+++ dbo.alumnos.sgl.sql
@@ -1,8 +1,8 @@
-﻿CREATE TABLE [dbo].[matriculas] (
+﻿ CREATE TABLE [dbo].[matriculas] (
     [idMatriula ] INT       NOT NULL,
     [idAlumno]    INT       NOT NULL,
     [fecha ]      DATE      NOT NULL,
     [periodo ]    CHAR (10) NULL,
     PRIMARY KEY CLUSTERED ([idMatriula ] ASC)
 );    
-
+  
