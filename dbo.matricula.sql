2dc2e2b04a84376ef1c9fe76861d6ea52d2a8fb4
paradaalma421-jpg <paradaalma421@gmail.com>
2026-09-25 19:47:22 -0600

    tabla matricula
    Removed unnecessary comments and SQLCMD settings from the script.

--- dbo.matricula.sql
+++ dbo.matricula.sql
@@ -1,46 +1,10 @@
-﻿ /*
-Script de implementación para db_Academca.mdf
-
-Una herramienta generó este código.
-Los cambios realizados en este archivo podrían generar un comportamiento incorrecto y se perderán si
-se vuelve a generar el código.
-*/
-
-GO
-SET ANSI_NULLS, ANSI_PADDING, ANSI_WARNINGS, ARITHABORT, CONCAT_NULL_YIELDS_NULL, QUOTED_IDENTIFIER ON;
-
-SET NUMERIC_ROUNDABORT OFF;
+CREATE TABLE [dbo].[matricula] (
+    [idMatricula] INT       NOT NULL,
+    [idAlumno ]   INT       NOT NULL,
+    [fecha]       DATE      NULL,
+    [periodo ]    CHAR (10) NULL,
+    PRIMARY KEY CLUSTERED ([idMatricula] ASC)
+);
 
 
-GO
-:setvar DatabaseName "db_Academca.mdf"
-:setvar DefaultFilePrefix "db_Academca.mdf"
-:setvar DefaultDataPath "C:\Users\DELL\AppData\Local\Microsoft\Microsoft SQL Server Local DB\Instances\ProjectsV13\"
-:setvar DefaultLogPath "C:\Users\DELL\AppData\Local\Microsoft\Microsoft SQL Server Local DB\Instances\ProjectsV13\"
 
-GO
-:on error exit
-GO
-/*
-Detectar el modo SQLCMD y deshabilitar la ejecución del script si no se admite el modo SQLCMD.
-Para volver a habilitar el script después de habilitar el modo SQLCMD, ejecute lo siguiente:
-SET NOEXEC OFF; 
-*/
-:setvar __IsSqlCmdEnabled "True"
-GO
-IF N'$(__IsSqlCmdEnabled)' NOT LIKE N'True'
-    BEGIN
-        PRINT N'El modo SQLCMD debe estar habilitado para ejecutar correctamente este script.';
-        SET NOEXEC ON;
-    END
-
-
-GO
-USE [$(DatabaseName)];
-
-
-GO
-PRINT N'Actualización completada.';
-
-
-GO
